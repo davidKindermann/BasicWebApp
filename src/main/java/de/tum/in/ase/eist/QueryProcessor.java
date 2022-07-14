26cf4f5f3396e45a9167f82428980bbcbc552dfa
ge64teg <ge64teg@mytum.de>
2022-07-14 13:24:00 +0200

    sum of numbers

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -6,14 +6,30 @@ import org.springframework.stereotype.Service;
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "David";
-        } else { // TODO extend the programm here
+            return "David";
+        } else if (query.contains("plus")) {
+            String[] strings = query.split(" ");
+            int counter = 0;
+            Double[] numbers = new Double[2];
+            for (int i = 0; i < strings.length; i++) {
+                try {
+                    double x = Double.parseDouble(strings[i]);
+                    numbers[counter] = x;
+                    counter++;
+                } catch (Exception e) {
+                    continue;
+                }
+            }
+            double sum = numbers[0] + numbers[1];
+            return "" + sum;
+        }// TODO extend the programm here
+        else {
             return "";
         }//hello
     }
